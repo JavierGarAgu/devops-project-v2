e22a8d199a11368423d4722d4ee536fee5c8ab40
javierGarAgu <javiergaragu@gmail.com>
2025-10-01 16:26:15 +0200

    deleting script updated

--- scripts/eks_delete.ps1
+++ scripts/eks_delete.ps1
@@ -6,12 +6,9 @@ $Folder2 = "..\iac\aws\delete_eks_private_nodegroup"
 
 # -------------------------
 Push-Location $Folder1
-terraform state rm kubectl_manifest.aws_auth
-terraform state rm kubernetes_namespace.arc
-terraform state rm helm_release.arc
-terraform state rm helm_release.my_runner
-terraform state rm kubernetes_secret.arc_github_token
+terraform destroy -target "aws_eks_node_group.private_ng" -auto-approve -var "github_token=1"
+terraform destroy -target "aws_eks_cluster.this" -auto-approve -var "github_token=1"
 terraform destroy -auto-approve
 Pop-Location
 
-Write-Host "All done! EKS resources destroyed first, then the rest of folder1 resources."
+Write-Host "EKS resources destroyed first then the rest of resources"
